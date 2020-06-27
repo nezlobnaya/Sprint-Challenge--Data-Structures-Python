f134a21d83fdf23676c25f99d9624ca82329b996
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2020-06-27 13:07:17 -0400

    reverse_list added

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -39,4 +39,11 @@ class LinkedList:
         return False
 
     def reverse_list(self, node, prev):
-        pass
+        elif node.next_node is None:
+            self.head = node
+            node.next_node = prev
+            return node
+        else:
+            next_n = node.next_node
+            node.next_node = prev
+            self.reverse_list(next_n, node)
